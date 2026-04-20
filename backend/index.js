f0245032d8f420278402d1d7967cf6f434e74e05
Ilyas Ahmed <ilyasahmed2727@gmail.com>
2026-04-20 09:24:56 -0400

    prep backend for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "crewpilot",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 const dbp = db.promise();
 
@@ -385,6 +386,7 @@ app.delete("/sent-days/:date", (req, res) => {
 // =========================
 // SERVER
 // =========================
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
