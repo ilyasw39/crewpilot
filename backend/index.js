f2b1e4f3f6f9248c0cb5898cba87a709fc7c5e73
Ilyas Ahmed <ilyasahmed2727@gmail.com>
2026-04-20 12:05:02 -0400

    add dotenv + latest backend fixes

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,13 @@ app.use(express.json());
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 const dbp = db.promise();
 
